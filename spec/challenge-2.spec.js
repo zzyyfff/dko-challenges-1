dc1a67e8f1f2d84717a4cfc9a9e02eebca761a13
dko-challenges <dko.challenges@gmail.com>
2019-05-11 12:11:28 -0400

    add challenge 2, and tests

--- spec/challenge-2.spec.js
+++ spec/challenge-2.spec.js
@@ -9,14 +9,18 @@ const expect = chai.expect
 
 const challenge = require('../lib/challenge-2.js')
 
-describe('challenge-2: correct number of months for $100', function () {
+const names0 = [ 'max john', 'lebron james', 'oprah', 'james harden', 'john wall', 'harden pokemon' ]
+
+const names1 = [ 'jimmy john', 'john jimmy', 'jimmy butler', 'butler mike', 'mike james', 'jimmy mike' ]
+
+describe('challenge-2: correct names for none repeating', function () {
   it('returns the correct value', function () {
-    expect(challenge(100)).to.equal(42)
+    expect(challenge(names0)).to.equal('lebron james harden')
   })
 })
 
-describe('challenge-2: short string of numbers', function () {
+describe('challenge-2: correct names for none repeating', function () {
   it('returns the correct value', function () {
-    expect(challenge('12353')).to.equal('33521')
+    expect(challenge(names1)).to.equal('jimmy john jimmy butler mike james')
   })
 })
